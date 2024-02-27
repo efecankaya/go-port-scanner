5267d3b6d3393dfb0368e27070db704d953b5fa1
efecankaya <efecankaya56@gmail.com>
2024-02-27 16:39:53 +0300

    feature: limit concurrency

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func scanPort(host string, port int, wg *sync.WaitGroup) {
+func scanPort(host string, port int, wg *sync.WaitGroup, limiter chan struct{}) {
 	defer wg.Done()
 	target := fmt.Sprintf("%s:%d", host, port)
 	conn, err := net.DialTimeout("tcp", target, 1*time.Second)
@@ -21,9 +21,14 @@ func scanPort(host string, port int, wg *sync.WaitGroup) {
 func main() {
 	host := "scanme.nmap.org"
 	var wg sync.WaitGroup
-	for port := 1; port <= 1024; port++ {
+	limiter := make(chan struct{}, 100) // Limit concurrency to 100 goroutines
+	for port := 1; port <= 65535; port++ {
 		wg.Add(1)
-		go scanPort(host, port, &wg)
+		limiter <- struct{}{} // Add token to limiter channel
+		go func(p int) {
+			defer func() { <-limiter }() // Remove token from limiter channel
+			scanPort(host, p, &wg, limiter)
+		}(port)
 	}
 	wg.Wait()
 }
