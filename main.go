b562c27db28539ba473ae31b39e932d0a3ce1164
efecankaya <efecankaya56@gmail.com>
2024-02-27 16:52:49 +0300

    feature: print banner information

--- main.go
+++ main.go
@@ -15,14 +15,27 @@ func scanPort(host string, port int, wg *sync.WaitGroup, limiter chan struct{}) 
 		return
 	}
 	defer conn.Close()
-	fmt.Printf("Port %d is open\n", port)
+
+	// Set a deadline for reading data from the connection
+	conn.SetReadDeadline(time.Now().Add(1 * time.Second))
+
+	// Attempt to read data from the connection
+	var buf [1024]byte
+	n, err := conn.Read(buf[:])
+	if err != nil {
+		// If no data is received, print that the port is open
+		fmt.Printf("Port %d is open.\n", port)
+		return
+	}
+
+	fmt.Printf("Port %d is open. Banner: %s\n", port, string(buf[:n]))
 }
 
 func main() {
 	host := "scanme.nmap.org"
 	var wg sync.WaitGroup
 	limiter := make(chan struct{}, 100) // Limit concurrency to 100 goroutines
-	for port := 1; port <= 65535; port++ {
+	for port := 1; port <= 1024; port++ {
 		wg.Add(1)
 		limiter <- struct{}{} // Add token to limiter channel
 		go func(p int) {
