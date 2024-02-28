7d898b10c65a4ee9050351ed49609395205f60f5
DepletedSandwich <erenyasar@sabanciuniv.edu>
2024-02-29 02:56:16 +0300

    "Bug Fix | | Multithread Load Done"

--- port_scanner.go
+++ port_scanner.go
@@ -10,16 +10,41 @@ import (
 	"time"
 )
 
-func scanPort(ip string, port_start int, port_end int, timeout time.Duration, wg *sync.WaitGroup) {
+func port_range_distribute(end_port int, start_port int, thread_count int) []int {
+	port_amount := (end_port - start_port) + 1
+	thread_load := port_amount / thread_count
+	thread_mod := port_amount % thread_count
+
+	//Create port load per thread slice
+	if thread_load == 0 {
+		port_range_dist := make([]int, thread_mod)
+		for i := 0; i < len(port_range_dist); i++ {
+			port_range_dist[i] = 1
+		}
+		return port_range_dist
+	} else {
+		port_range_dist := make([]int, thread_count)
+		for i := 0; i < thread_count; i++ {
+			if thread_mod > 0 {
+				port_range_dist[i] = thread_load + 1
+				thread_mod -= 1
+			} else {
+				port_range_dist[i] = thread_load
+			}
+		}
+		return port_range_dist
+	}
+}
+func scanPort(ip string, port_start int, scan_amount int, timeout time.Duration, wg *sync.WaitGroup) {
 	defer wg.Done()
-	for scan_port := port_start; scan_port < port_end; scan_port++ {
+	for scan_port := port_start; scan_port < port_start+scan_amount; scan_port++ {
 		target := fmt.Sprintf("%s:%d", ip, scan_port)
-		conn, err := net.DialTimeout("tcp", target, timeout)
+		conn, err := net.DialTimeout("tcp", target, timeout) //Exception Handling --> Port maybe not availiable
 		if err != nil {
 			continue
 		}
 		fmt.Printf("Port %d is open\n", scan_port)
-		defer conn.Close()
+		conn.Close()
 	}
 }
 
@@ -41,11 +66,12 @@ func main() {
 	IPaddress, err := net.LookupHost(usr_domain_input)
 	if err != nil || usr_domain_input == "" { //Cannot resolve domain name
 		fmt.Println("Invalid domain name!")
+		flag.Usage()
 		return
 	}
 	fmt.Printf("The IP address of host is %s\n", IPaddress[0])
 
-	if thread_count < 0 || thread_count > 100 { //Limit threads
+	if thread_count < 0 || thread_count > 300 { //Limit threads
 		fmt.Println("Thread count violation!")
 		return
 	}
@@ -59,29 +85,25 @@ func main() {
 
 	start_port, err_startp := strconv.Atoi(port_span[0])
 	end_port, err_endp := strconv.Atoi(port_span[1])
-
 	if err_startp != nil || err_endp != nil {
 		fmt.Println("Error: Limit not integer defined!")
 		return
 	}
-	if start_port > end_port {
-		fmt.Println("Error: Start port cannot be greater than ending port!")
+
+	if start_port < 1 || end_port < 1 || start_port > end_port {
+		fmt.Println("Error: Invalid Port Range!")
 		return
 	}
 
 	//Give threads the port scan loads && Assign threads their port load
-	thread_load := (end_port - start_port) / thread_count
-	thread_mod := (end_port - start_port) % thread_count
+	port_range_dist := port_range_distribute(end_port, start_port, thread_count)
 	timeout := time.Second
 
-	for i := 0; i < thread_count; i++ {
+	for i := 0; i < len(port_range_dist); i++ {
 		wg.Add(1)
-		if start_port+thread_load < end_port {
-			go scanPort(IPaddress[0], start_port, start_port+thread_load, timeout, &wg)
-			start_port += thread_load
-		} else {
-			go scanPort(IPaddress[0], start_port, start_port+thread_mod, timeout, &wg)
-		}
+		go scanPort(IPaddress[0], start_port, port_range_dist[i], timeout, &wg)
+		start_port += port_range_dist[i]
 	}
+
 	wg.Wait()
 }
